90683a599f8ad88a81682db10a978291baca4862
Abhishek Shukla <abhi.shukla0047@gmail.com>
2020-09-15 23:55:49 -0700

    Updates

---
+++ precache-manifest.51582d02c97fce187a0c9c317b4fea91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d4c54c677b3257c26c2e917564ef80",
+    "url": "/PixabayApi/index.html"
+  },
+  {
+    "revision": "71e8df79298931aa9452",
+    "url": "/PixabayApi/static/js/2.25eda822.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/PixabayApi/static/js/2.25eda822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e78e1e1c77ae4b551b",
+    "url": "/PixabayApi/static/js/main.8c8c9fb7.chunk.js"
+  },
+  {
+    "revision": "fe4bc4d0af8afe4781a6",
+    "url": "/PixabayApi/static/js/runtime-main.27195e5c.js"
+  }
+]);
